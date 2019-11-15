2a59d4f499935e2a907546a038418062ae550276
Jean-Christophe Vassort <jc.vassort@open-web.fr>
2019-11-15 09:26:38 +0100

    Fix: use prepared statement to prevent SQL injection

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 $server_user = $_POST['user'];
 $server_hostname = $_POST['hostname'];
 
-$mysqli = new mysqli(
-  $_ENV['MYSQL_HOST'],
+// https://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
+
+$db = new PDO(
+  sprintf('mysql:dbname=%s;host=%s;charset=utf8', $_ENV['MYSQL_DATABASE'], $_ENV['MYSQL_HOST']),
   $_ENV['MYSQL_USER'],
-  $_ENV['MYSQL_PASSWORD'],
-  $_ENV['MYSQL_DATABASE']);
+  $_ENV['MYSQL_PASSWORD']);
+
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $query = "-- Search per server
 SELECT public_key FROM ssh_keys
 INNER JOIN link_servers ON ssh_keys.ssh_user = link_servers.ssh_user_username
-WHERE link_servers.server_user = '${server_user}'
-AND link_servers.server_hostname = '${server_hostname}'
+WHERE link_servers.server_user = :server_user_1
+AND link_servers.server_hostname = :server_hostname_1
 AND (
   (CURDATE() BETWEEN link_servers.valid_from AND link_servers.valid_until)
   OR
@@ -25,8 +29,8 @@ UNION
 SELECT public_key FROM ssh_keys
 INNER JOIN link_server_groups ON ssh_keys.ssh_user = link_server_groups.ssh_user_username
 INNER JOIN servers_groups_link ON link_server_groups.groupname = servers_groups_link.server_groupname
-WHERE link_server_groups.server_user = '${server_user}'
-AND servers_groups_link.server_hostname = '${server_hostname}'
+WHERE link_server_groups.server_user = :server_user_2
+AND servers_groups_link.server_hostname = :server_hostname_2
 AND (
   (CURDATE() BETWEEN link_server_groups.valid_from AND link_server_groups.valid_until)
   OR
@@ -45,9 +49,17 @@ AND (
   (link_server_groups.valid_until IS NULL AND link_server_groups.valid_from IS NULL)
 )";
 
-$result = $mysqli->query($query);
+$stmt = $db->prepare($query);
+$stmt->execute(
+  array(
+    'server_user_1' => $server_user,
+    'server_hostname_1' => $server_hostname,
+    'server_user_2' => $server_user,
+    'server_hostname_2' => $server_hostname,
+  )
+);
 
-while($row = $result->fetch_assoc()) {
+foreach($stmt as $row) {
     echo $row['public_key'] . PHP_EOL;
 }
 
